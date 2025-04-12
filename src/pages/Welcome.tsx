9abb14fca0b2de97bd1d5cd38b0f8a429d88cd4c
rigorion <rigorionplc@gmail.com>
2025-04-12 02:35:36 +0200

    Update Welcome.tsx

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -202,7 +202,7 @@ const Index = () => {
                 className="h-20 w-10 text-blue-dark/70" 
                 fill="none" 
                 viewBox="0 0 42 42" 
-                stroke="currentColor"
+                stroke="black"
               >
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
