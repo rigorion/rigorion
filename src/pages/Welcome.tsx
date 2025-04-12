c48f50f8f1ee43542fcdfe798688fd6fa124297d
rigorion <rigorionplc@gmail.com>
2025-04-12 02:25:12 +0200

    Update Welcome.tsx

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -195,13 +195,13 @@ const Index = () => {
             ))}
           </div>
         ) : filteredModules.length === 0 ? (
-          <div className="flex flex-col items-center justify-center py-16 text-center">
+          <div className="flex flex-col items-center justify-center py-12 text-center">
             <div className="bg-blue-light/30 p-5 rounded-full mb-4">
               <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 className="h-20 w-10 text-blue-dark/70" 
                 fill="none" 
-                viewBox="0 0 24 24" 
+                viewBox="0 0 42 42" 
                 stroke="currentColor"
               >
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
