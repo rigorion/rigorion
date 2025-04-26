ed3cc22ea97568cb67e95e5814187c1b3797aa83
rigorion <rigorionplc@gmail.com>
2025-04-26 23:36:19 +0200

    Update TestMocksList.tsx

--- src/components/progress/TestMocksList.tsx
+++ src/components/progress/TestMocksList.tsx
@@ -4,10 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
 import { cn } from "@/lib/utils";
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_ANON_KEY!
-);
+const supabase = createClient('https://eantvimmgdmxzwrjwrop.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhbnR2aW1tZ2RteHp3cmp3cm9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2MTA5MjQsImV4cCI6MjA2MTE4NjkyNH0.ba6UMchrXuMqmkWXzoC2Dd91y-HJm_cB1NMmwNRly-k')
+const { data, error } = await supabase.functions.invoke('get-user-progress', {
+  body: { name: 'Functions' },
+})
 
 interface TestMockData {
   id: string;
